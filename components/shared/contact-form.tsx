7164359c223f02090ef1ac7752f245f75b3f9057
George Afrifa <sarpongk247@gmail.com>
2024-10-22 06:29:24 +0000

    updates and changes

--- components/shared/contact-form.tsx
+++ components/shared/contact-form.tsx
@@ -1,139 +1,87 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
-import schoolLogoLarge from "../../public/images/logo-large.webp";
 
 export default function ContactForm() {
   return (
     <section
       id="contact_form"
-      className="relative z-20 w-full bg-white py-14 md:py-20"
+      className="relative z-20 h-auto md:h-[45rem] w-full "
     >
-      <div className="w-full container flex-between flex-col md:flex-row gap-10 md:gap-0">
-        <div className="w-full">
-          <div className="flex items-center gap-1">
-            <Image src={schoolLogoLarge} alt="logo" height={130} width={130} />
-            <div className="space-y-4">
-              <p className="text-base md:text-lg text-emerald-600 leading-none">
-                <span className="font-bold">NURSING AND MIDWIFERY</span> <br />
-                Training College, Kumasi
-              </p>
-              <h3 className="text-sm md:text-base uppercase font-medium italic text-emerald-900">
-                &quot;enter to learn and go out to serve&quot;
-              </h3>
-            </div>
-          </div>
+      <div className="h-full w-full grid grid-cols-1 md:grid-cols-3 p-0 md:p-5 gap-2.5">
+        <div className="hidden md:block w-full h-full bg-slate-50 shadow rounded-lg">
+          some info goes here
         </div>
-        <div className="w-full bg-emerald-50/80 rounded-lg shadow-md px-5 md:px-14 py-10 md:py-14">
-          <h3 className="text-lg md:text-2xl font-semibold mb-2">
-            Get in Touch
-          </h3>
-          <Separator className="mb-5" />
-          <div className="w-full">
-            <form className="w-full">
-              <div className="relative z-0 w-full mb-5 group">
+        <div className="w-full h-full bg-emerald-50 shadow md:col-span-2 rounded-none md:rounded-lg px-5 md:px-0 py-10">
+          <div className="w-full h-full center">
+            <form className="w-full md:w-[28.5rem]">
+              <div className="mb-5">
+                <label
+                  htmlFor="full_name"
+                  className="block mb-2 text-sm font-medium text-gray-900"
+                >
+                  Full name
+                </label>
                 <input
-                  type="email"
-                  name="floating_email"
-                  id="floating_email"
-                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                  placeholder=" "
+                  type="text"
+                  id="full_name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                  placeholder="John Doe"
                   required
                 />
-                <label
-                  htmlFor="floating_email"
-                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                >
-                  Email address
-                </label>
               </div>
 
-              <div className="grid md:grid-cols-2 md:gap-6">
-                <div className="relative z-0 w-full mb-5 group">
+              <div className="grid md:grid-cols-2 gap-5 mb-5">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    Email address
+                  </label>
                   <input
-                    type="text"
-                    name="floating_first_name"
-                    id="floating_first_name"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
+                    type="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="john.doe@company.com"
                     required
                   />
-                  <label
-                    htmlFor="floating_first_name"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    First name
-                  </label>
                 </div>
-                <div className="relative z-0 w-full mb-5 group">
-                  <input
-                    type="text"
-                    name="floating_last_name"
-                    id="floating_last_name"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
-                    required
-                  />
+                <div>
                   <label
-                    htmlFor="floating_last_name"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                    htmlFor="phone"
+                    className="block mb-2 text-sm font-medium text-gray-900"
                   >
-                    Last name
+                    Phone number
                   </label>
-                </div>
-              </div>
-              <div className="grid md:grid-cols-2 md:gap-6">
-                <div className="relative z-0 w-full mb-5 group">
                   <input
                     type="tel"
+                    id="phone"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="024-123-4567"
                     pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                    name="floating_phone"
-                    id="floating_phone"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
                     required
                   />
-                  <label
-                    htmlFor="floating_phone"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Phone number (123-456-7890)
-                  </label>
-                </div>
-                <div className="relative z-0 w-full mb-5 group">
-                  <input
-                    type="text"
-                    name="floating_company"
-                    id="floating_company"
-                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                    placeholder=" "
-                    required
-                  />
-                  <label
-                    htmlFor="floating_company"
-                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-                  >
-                    Company (Ex. Google)
-                  </label>
                 </div>
               </div>
+
               <div className="relative z-0 w-full mb-5 group">
                 <label
                   htmlFor="message"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Your message
                 </label>
                 <textarea
                   id="message"
-                  rows={7}
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  rows={10}
+                  className="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Write your thoughts here..."
                 ></textarea>
               </div>
-              <Button type="submit" className="flex items-center text-sm">
-                Send message
-              </Button>
+              <div className="flex items-center justify-end">
+                <Button type="submit" className="flex items-center">
+                  Send message
+                </Button>
+              </div>
             </form>
           </div>
         </div>
