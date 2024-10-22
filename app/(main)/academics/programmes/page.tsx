8f203a7d8e13a3bfdc0f203ee50faebf6eb40eb6
George Sarpong Afrifa <sarpongk247@gmail.com>
2024-10-22 13:35:19 +0000

    updates

--- app/(main)/academics/programmes/page.tsx
+++ app/(main)/academics/programmes/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Page() {
-  return <div>Page</div>;
+  return (
+    <>
+      <section className="sticky top-16 z-10 w-full bg-black h-40 md:h-80 center">
+        <div className="text-white left-0 right-0 mx-auto">programmes</div>
+      </section>
+      <div className="bg-white relative z-20">
+        <section>Some informations</section>
+      </div>
+    </>
+  );
 }
--- components/shared/contact-form.tsx
+++ components/shared/contact-form.tsx
@@ -1,91 +1,92 @@
-import { Button } from "@/components/ui/button";
-
 export default function ContactForm() {
   return (
-    <section
-      id="contact_form"
-      className="relative z-20 h-auto md:h-[45rem] w-full "
-    >
-      <div className="h-full w-full grid grid-cols-1 md:grid-cols-3 p-0 md:p-5 gap-2.5">
-        <div className="hidden md:block w-full h-full bg-slate-50 shadow rounded-lg">
-          some info goes here
-        </div>
-        <div className="w-full h-full bg-emerald-50 shadow md:col-span-2 rounded-none md:rounded-lg px-5 md:px-0 py-10">
-          <div className="w-full h-full center">
-            <form className="w-full md:w-[28.5rem]">
-              <div className="mb-5">
-                <label
-                  htmlFor="full_name"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Full name
-                </label>
-                <input
-                  type="text"
-                  id="full_name"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  placeholder="John Doe"
-                  required
-                />
-              </div>
+    // <section
+    //   id="contact_form"
+    //   className="relative z-20 h-auto md:h-[45rem] w-full "
+    // >
+    //   <div className="h-full w-full grid grid-cols-1 md:grid-cols-3 p-0 md:p-5 gap-2.5">
+    //     <div className="hidden md:block w-full h-full bg-slate-50 shadow rounded-lg">
+    //       some info goes here
+    //     </div>
+    //     <div className="w-full h-full bg-emerald-50 shadow md:col-span-2 rounded-none md:rounded-lg px-5 md:px-0 py-10">
+    //       <div className="w-full h-full center">
+    //         <form className="w-full md:w-[28.5rem]">
+    //           <div className="mb-5">
+    //             <label
+    //               htmlFor="full_name"
+    //               className="block mb-2 text-sm font-medium text-gray-900"
+    //             >
+    //               Full name
+    //             </label>
+    //             <input
+    //               type="text"
+    //               id="full_name"
+    //               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+    //               placeholder="John Doe"
+    //               required
+    //             />
+    //           </div>
 
-              <div className="grid md:grid-cols-2 gap-5 mb-5">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="john.doe@company.com"
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="phone"
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                  >
-                    Phone number
-                  </label>
-                  <input
-                    type="tel"
-                    id="phone"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="024-123-4567"
-                    pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                    required
-                  />
-                </div>
-              </div>
+    //           <div className="grid md:grid-cols-2 gap-5 mb-5">
+    //             <div>
+    //               <label
+    //                 htmlFor="email"
+    //                 className="block mb-2 text-sm font-medium text-gray-900"
+    //               >
+    //                 Email address
+    //               </label>
+    //               <input
+    //                 type="email"
+    //                 id="email"
+    //                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+    //                 placeholder="john.doe@company.com"
+    //                 required
+    //               />
+    //             </div>
+    //             <div>
+    //               <label
+    //                 htmlFor="phone"
+    //                 className="block mb-2 text-sm font-medium text-gray-900"
+    //               >
+    //                 Phone number
+    //               </label>
+    //               <input
+    //                 type="tel"
+    //                 id="phone"
+    //                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+    //                 placeholder="024-123-4567"
+    //                 pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+    //                 required
+    //               />
+    //             </div>
+    //           </div>
 
-              <div className="relative z-0 w-full mb-5 group">
-                <label
-                  htmlFor="message"
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                  Your message
-                </label>
-                <textarea
-                  id="message"
-                  rows={10}
-                  className="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-                  placeholder="Write your thoughts here..."
-                ></textarea>
-              </div>
-              <div className="flex items-center justify-end">
-                <Button type="submit" className="flex items-center">
-                  Send message
-                </Button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
+    //           <div className="relative z-0 w-full mb-5 group">
+    //             <label
+    //               htmlFor="message"
+    //               className="block mb-2 text-sm font-medium text-gray-900"
+    //             >
+    //               Your message
+    //             </label>
+    //             <textarea
+    //               id="message"
+    //               rows={10}
+    //               className="block resize-none p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+    //               placeholder="Write your thoughts here..."
+    //             ></textarea>
+    //           </div>
+    //           <div className="flex items-center justify-end">
+    //             <Button type="submit" className="flex items-center">
+    //               Send message
+    //             </Button>
+    //           </div>
+    //         </form>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <section className="relative z-20 w-full h-[30rem] bg-emerald-200 py-10">
+      <div className="center">Some content</div>
     </section>
   );
 }
