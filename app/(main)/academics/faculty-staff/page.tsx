78c1b116f11cd6b8fd7c1c484d9a8d0c217aa9d2
George Afrifa <sarpongk247@gmail.com>
2024-10-22 15:52:04 +0000

    staff ui update

--- app/(main)/academics/faculty-staff/page.tsx
+++ app/(main)/academics/faculty-staff/page.tsx
@@ -1,5 +1,118 @@
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
 export default function Page() {
-  return <div>Page</div>;
+  interface Management {
+    position: string;
+    name: string;
+    email: string;
+  }
+
+  const managementData: Management[] = [
+    {
+      position: "Principal",
+      name: "Joana Owusu-Danso",
+      email: "jo6danso@gmail.com",
+    },
+    {
+      position: "Vice Principal",
+      name: "Veronica Oduro-Kwarteng",
+      email: "ronikwart@yahoo.com",
+    },
+    {
+      position: "Programme Head, RGN",
+      name: "Yussif Issah Sumaila",
+      email: "sumailay@yahoo.com",
+    },
+    {
+      position: "Programme Head, RM",
+      name: "Dorcas Fati Asare",
+      email: "dasaref9@gmail.com",
+    },
+    {
+      position: "Administrator",
+      name: "Theophilus H. Azaanaang",
+      email: "tazaanaang@gmail.com",
+    },
+    {
+      position: "Head of Finance",
+      name: "Collins Amankwah",
+      email: "collyman20@yahoo.com",
+    },
+    {
+      position: "Head of Procurement",
+      name: "Kennedy Nii Lamptey",
+      email: "kennman@gmail.com",
+    },
+    {
+      position: "Matron",
+      name: "Grace Antwi",
+      email: "grace.antwi@gmail.com",
+    },
+    {
+      position: "Librarian",
+      name: "Ransford Sarfo-Mensah",
+      email: "rsarfomensah@gmail.com",
+    },
+    {
+      position: "Hostel Warden",
+      name: "Veronica Asamoah",
+      email: "vasamoah@gmail.com",
+    },
+  ];
+  return (
+    <>
+      <section className="sticky top-16 z-10 w-full bg-black h-40 md:h-80 center">
+        <div className="text-white left-0 right-0 mx-auto">Meet the Team</div>
+      </section>
+      <div className="relative z-20 bg-white py-10 space-y-10">
+        <div className="container space-y-10">
+          <h2 className="text-center font-bold text-3xl">
+            The Management Team
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
+            {managementData.map((manager) => (
+              <div key={manager.email} className="h-96">
+                <div className="h-2/3 bg-green-500"></div>
+                <div className="h-1/3 py-4">
+                  <p className="text-xs font-light mb-2">{manager.position}</p>
+                  <h3 className="text-slate-900 font-semibold text-xl">
+                    {manager.name}
+                  </h3>
+                  <a
+                    href={`mailTo:${manager.email}`}
+                    className="text-sm underline"
+                  >
+                    {manager.email}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+          <Separator />
+          <h2 className="text-center font-bold text-3xl">
+            The Faculty Members
+          </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+            {[...new Array(20)].map((_, idx) => (
+              <div key={idx} className="h-32 bg-red-500 group cursor-pointer">
+                <div className="h-full p-4 bg-neutral-100">
+                  <p className="text-sm mb-1 font-light">Principal</p>
+                  <h3 className="text-slate-900 group-hover:text-emerald-500 transition-colors duration-200 font-semibold text-2xl">
+                    Mrs. Joana Owusu-Danso
+                  </h3>
+                  <a
+                    href="mailTo:jo6danso@gmail.com"
+                    className="text-sm underline font-medium"
+                  >
+                    jo6danso@gmail.com
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- app/(main)/academics/programmes/page.tsx
+++ app/(main)/academics/programmes/page.tsx
@@ -1,13 +1,92 @@
+import { Separator } from "@/components/ui/separator";
 import React from "react";
 
+type CollegePassage = {
+  topic: string;
+  content: string;
+};
+
 export default function Page() {
+  const nursingPassages: CollegePassage[] = [
+    {
+      topic: "Online Education",
+      content:
+        "At the Nursing and Midwifery Training College, Kumasi, we embrace the power of online education to enhance learning flexibility and accessibility. Our innovative e-learning platform allows students to engage with course materials, participate in discussions, and collaborate with peers from anywhere. This modern approach ensures that aspiring nurses and midwives can balance their studies with personal and professional commitments while receiving a high-quality education.",
+    },
+    {
+      topic: "Degrees",
+      content:
+        "Our college offers comprehensive degree programs designed to equip students with the essential knowledge and skills needed for a successful career in nursing and midwifery. Each degree program combines rigorous academic coursework with practical, hands-on training in clinical settings. Graduates emerge as competent healthcare professionals, ready to meet the diverse needs of patients and communities.",
+    },
+    {
+      topic: "Certificates",
+      content:
+        "Nursing and Midwifery Training College, Kumasi, provides various certificate programs tailored for those seeking to enhance their qualifications or specialize in specific areas of healthcare. These certificates offer targeted training in skills such as maternal and child health, geriatric care, and emergency response. They serve as a valuable asset for career advancement and deepen students’ understanding of critical healthcare practices.",
+    },
+    {
+      topic: "Career Training",
+      content:
+        "Our commitment to career training at the Nursing and Midwifery Training College, Kumasi, ensures that students receive the practical experience necessary to thrive in the healthcare environment. Through clinical placements and simulated learning experiences, students develop critical thinking, communication, and technical skills. Our dedicated faculty guides them in navigating their career paths, preparing them for various roles in nursing and midwifery.",
+    },
+    {
+      topic: "Community Opportunities",
+      content:
+        "Community engagement is a cornerstone of education at the Nursing and Midwifery Training College, Kumasi. Students actively participate in outreach programs that provide healthcare services to underserved populations. These opportunities not only enhance students’ practical skills but also foster a strong sense of social responsibility and compassion, vital attributes for any healthcare professional.",
+    },
+    {
+      topic: "Academic Excellence",
+      content:
+        "At Nursing and Midwifery Training College, Kumasi, we uphold a standard of academic excellence that empowers our students to excel in their studies and future careers. Our dedicated faculty, comprehensive curriculum, and supportive learning environment motivate students to achieve their full potential. We celebrate the achievements of our graduates, who consistently perform at high levels in national examinations and make significant contributions to the healthcare field.",
+    },
+  ];
   return (
     <>
       <section className="sticky top-16 z-10 w-full bg-black h-40 md:h-80 center">
         <div className="text-white left-0 right-0 mx-auto">programmes</div>
       </section>
-      <div className="bg-white relative z-20">
-        <section>Some informations</section>
+      <div className="relative bg-white z-20 center flex-col">
+        <section className="w-full h-full py-10 bg-slate-100 center">
+          <div className="w-full md:w-[75rem] h-auto md:h-[15rem] grid grid-cols-1 md:grid-cols-3 gap-5 px-5 md:px-0 *:bg-white *:shadow-md *:p-8 *:flex *:flex-col *:justify-center">
+            <div className="space-y-2">
+              <h3 className="text-emerald-600 font-light text-xl md:text-2xl">
+                01.
+              </h3>
+              <h1 className="text-3xl md:text-4xl font-medium text-slate-800">
+                Registered General Nursing (RGN)
+              </h1>
+            </div>
+            <div className="space-y-2">
+              <h3 className="text-emerald-600 font-light text-xl md:text-2xl">
+                02.
+              </h3>
+              <h1 className="text-3xl md:text-4xl font-medium text-slate-800">
+                Registered Midwifery (RM)
+              </h1>
+            </div>
+            <div className="space-y-2">
+              <h3 className="text-emerald-600 font-light text-xl md:text-2xl">
+                03.
+              </h3>
+              <h1 className="text-3xl md:text-4xl font-medium text-slate-800">
+                Post NAC/NAP (PNN)
+              </h1>
+            </div>
+          </div>
+        </section>
+        <section className="w-full p-5 md:p-20 center bg-neutral-100">
+          <div className="w-full md:w-[75rem] grid grid-cols-1 md:grid-cols-3 gap-5">
+            {nursingPassages.map((item, idx) => (
+              <div
+                key={idx}
+                className="w-full min-h-[13rem] px-4 py-5 space-y-2 border border-input rounded-md shadow-md bg-white"
+              >
+                <h3 className="text-base font-medium">{item.topic}</h3>
+                <Separator />
+                <p className="text-sm text-muted-foreground">{item.content}</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
