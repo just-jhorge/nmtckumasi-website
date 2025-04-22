066f5ecfa07684929f4878b4292751ecee4c041a
George Afrifa <sarpongk247@gmail.com>
2025-04-22 13:22:49 +0000

    updates and changes

--- app/admissions/interview-details/page.tsx
+++ app/admissions/interview-details/page.tsx
@@ -10,11 +10,7 @@ export default function Page() {
     <section className="w-full py-5 md:py-10">
       <div className="container max-w-4xl mx-auto space-y-14">
         <div className="py-40 text-center font-bold text-4xl">
-          ADMISSION FOR 2025/2026 ACADEMIC YEAR IS{" "}
-          <span className="text-destructive underline underline-offset-2">
-            NOT
-          </span>{" "}
-          OPENED YET!
+          ADMISSION FOR 2025/2026 ACADEMIC YEAR IS NOW OPENED
         </div>
         {/* <div className="text-sm md:text-base">
           <p className="mb-5">
