6e3d8d6bb79c2963713daf981370dfc0aded9ee0
George Afrifa <gsafrifa@gmail.com>
2025-09-12 19:05:45 +0000

    removed admission list

--- app/admissions/admissions-list/page.tsx
+++ app/admissions/admissions-list/page.tsx
@@ -1,43 +1,14 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-import AdmissionsList from "./admissions-list";
-import { kyInstance } from "@/lib/ky";
-import { Admitted } from "@prisma/client";
 import { Button } from "@/components/ui/button";
-import { Loader } from "lucide-react";
+import Link from "next/link";
 
 export default function Page() {
-  const { data, isFetching, error, refetch } = useQuery({
-    queryKey: ["admittedApplicants"],
-    queryFn: () => kyInstance.get("/api/admitted-list").json<Admitted[]>(),
-    retry: 3,
-  });
-
-  if (isFetching) {
-    return (
-      <div className="h-[70svh] container flex items-center justify-center">
-        <Loader className="size-4 animate-spin mr-2" /> Fetching data..
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex flex-col items-center justify-center gap-3">
-        <p>Something went wrong!</p>
-        <Button onClick={() => refetch()}>Refresh</Button>
-      </div>
-    );
-  }
-
-  if (!data) {
-    return (
-      <div className="h-[70svh] container flex items-center justify-center">
-        No data found
-      </div>
-    );
-  }
-
-  return <AdmissionsList applicants={data} />;
+  return (
+    <div className="w-full min-h-[65svh] flex items-center justify-center">
+      <Button asChild>
+        <Link href="/docs/instructional-sheet.pdf" download>
+          Download Instructional Sheet
+        </Link>
+      </Button>
+    </div>
+  );
 }
