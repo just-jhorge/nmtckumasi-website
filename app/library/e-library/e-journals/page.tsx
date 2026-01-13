f206b28438e4e6026845956f6569a5b4a5d2da6a
George S. Afrifa <gsafrifa@gmail.com>
2026-01-13 18:25:50 +0000

    added journal images

--- app/library/e-library/e-journals/page.tsx
+++ app/library/e-library/e-journals/page.tsx
@@ -1,36 +1,63 @@
 import ResourceCard from "../ResourceCard";
 
+import bmc from "../../../../public/images/library_images/bmc.png";
+import doaj from "../../../../public/images/library_images/doaj.png";
+import doab from "../../../../public/images/library_images/doab.png";
+import eldis from "../../../../public/images/library_images/eldis_plain.png";
+import fullpdf from "../../../../public/images/library_images/free_full_pdf.webp";
+import gscholar from "../../../../public/images/library_images/g_scholar.jpg";
+import hirewire from "../../../../public/images/library_images/highwire.png";
+import hindawi from "../../../../public/images/library_images/hindawi.png";
+import intech from "../../../../public/images/library_images/intech.png";
+import jstage from "../../../../public/images/library_images/j_stage.png";
+import msd from "../../../../public/images/library_images/msd.png";
+import nap from "../../../../public/images/library_images/nap.png";
+import opened from "../../../../public/images/library_images/open-edition-books.png";
+import paperity from "../../../../public/images/library_images/paperity.png";
+import plosone from "../../../../public/images/library_images/plos.png";
+import pubmed from "../../../../public/images/library_images/pub_med.png";
+import regate from "../../../../public/images/library_images/research_gate.png";
+import sageopen from "../../../../public/images/library_images/sage.jpeg";
+import sciencedirect from "../../../../public/images/library_images/sciencedirect.png";
+import scielo from "../../../../public/images/library_images/scielo.avif";
+import ssrn from "../../../../public/images/library_images/ssrn.png";
+import springeropen from "../../../../public/images/library_images/springeropen.png";
+import tayfran from "../../../../public/images/library_images/taylor_and_francis.png";
+import trip from "../../../../public/images/library_images/trip.png";
+import uptodate from "../../../../public/images/library_images/upto_date.png";
+import jws from "../../../../public/images/library_images/jws.png";
+
 export default function Page() {
   const journals = [
     {
       id: 1,
       name: "British Medical Central (BMC)",
       href: "https://www.biomedcentral.com",
-      imageSrc: "",
+      imageSrc: bmc,
     },
     {
       id: 2,
       name: "Directory of Open Access Journals (DOAJ)",
       href: "https://doaj.org",
-      imageSrc: "",
+      imageSrc: doaj,
     },
     {
       id: 3,
       name: "Directory of Open Access Repositories (OpenDOAR)",
       href: "https://v2.sherpa.ac.uk/opendoar/",
-      imageSrc: "",
+      imageSrc: doab,
     },
     {
       id: 4,
       name: "Electronic Development Information System (Eldis)",
       href: "https://www.eldis.org",
-      imageSrc: "",
+      imageSrc: eldis,
     },
     {
       id: 5,
       name: "Full PDF",
       href: "https://www.freefullpdf.com/#gsc.tab=0",
-      imageSrc: "",
+      imageSrc: fullpdf,
     },
     {
       id: 6,
@@ -42,133 +69,133 @@ export default function Page() {
       id: 7,
       name: "Google Scholar",
       href: "https://scholar.google.com",
-      imageSrc: "",
+      imageSrc: gscholar,
     },
     {
       id: 8,
       name: "HighWire Free Back Files",
       href: "http://portal.highwire.org/lists/freeart.dtl",
-      imageSrc: "",
+      imageSrc: hirewire,
     },
     {
       id: 9,
       name: "HighWire Free to Developing Economies",
       href: "http://portal.highwire.org/lists/devecon.dtl",
-      imageSrc: "",
+      imageSrc: hirewire,
     },
     {
       id: 10,
       name: "Hindawi",
       href: "https://www.hindawi.com",
-      imageSrc: "",
+      imageSrc: hindawi,
     },
     {
       id: 11,
       name: "InTech Open",
       href: "https://www.intechopen.com",
-      imageSrc: "",
+      imageSrc: intech,
     },
     {
       id: 12,
       name: "Japan Science and Technology Aggregator (J-STAGE)",
       href: "https://www.jstage.jst.go.jp/browse",
-      imageSrc: "",
+      imageSrc: jstage,
     },
     {
       id: 13,
       name: "MSD Manuals",
       href: "https://www.msdmanuals.com/professional?utm_campaign=2022partnerships&utm_medium=Linking&utm_source=KwameNkrumahUniversityofScienceandTechnology",
-      imageSrc: "",
+      imageSrc: msd,
     },
     {
       id: 14,
       name: "National Academies Press",
       href: "https://nap.nationalacademies.org",
-      imageSrc: "",
+      imageSrc: nap,
     },
     {
       id: 15,
       name: "OpenEdition Journals",
       href: "https://www.openedition.org/catalogue-journals",
-      imageSrc: "",
+      imageSrc: opened,
     },
     {
       id: 16,
       name: "Paperity",
       href: "https://paperity.org",
-      imageSrc: "",
+      imageSrc: paperity,
     },
     {
       id: 17,
       name: "PLOS ONE",
       href: "https://journals.plos.org/plosone/",
-      imageSrc: "",
+      imageSrc: plosone,
     },
     {
       id: 18,
       name: "PubMed Central (PMC)",
       href: "https://www.ncbi.nlm.nih.gov/pmc/",
-      imageSrc: "",
+      imageSrc: pubmed,
     },
     {
       id: 19,
       name: "ResearchGate",
       href: "https://www.researchgate.net",
-      imageSrc: "",
+      imageSrc: regate,
     },
     {
       id: 20,
       name: "SAGE Open",
       href: "https://journals.sagepub.com/home/sgo",
-      imageSrc: "",
+      imageSrc: sageopen,
     },
     {
       id: 21,
       name: "ScienceDirect (Open Access Content)",
       href: "https://www.sciencedirect.com/",
-      imageSrc: "",
+      imageSrc: sciencedirect,
     },
     {
       id: 22,
       name: "Scientific Electronic Library Online (SciELO)",
       href: "https://scielo.org",
-      imageSrc: "",
+      imageSrc: scielo,
     },
     {
       id: 23,
       name: "Social Science Research Network (SSRN)",
       href: "https://www.ssrn.com/index.cfm/en/",
-      imageSrc: "",
+      imageSrc: ssrn,
     },
     {
       id: 24,
       name: "SpringerOpen",
       href: "https://www.springeropen.com",
-      imageSrc: "",
+      imageSrc: springeropen,
     },
     {
       id: 25,
       name: "Taylor & Francis Open",
       href: "https://www.tandfonline.com/openaccess",
-      imageSrc: "",
+      imageSrc: tayfran,
     },
     {
       id: 26,
       name: "Trip Database",
       href: "https://www.tripdatabase.com",
-      imageSrc: "",
+      imageSrc: trip,
     },
     {
       id: 27,
       name: "UpToDate",
       href: "https://www.uptodate.com/login",
-      imageSrc: "",
+      imageSrc: uptodate,
     },
     {
       id: 28,
       name: "WorldWideScience",
       href: "https://worldwidescience.org",
-      imageSrc: "",
+      imageSrc: jws,
     },
   ];
 
